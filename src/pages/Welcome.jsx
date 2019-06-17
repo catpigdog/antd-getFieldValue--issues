725c5a22953bb7cc044d67c669a683267916e72d
大白 <1123603312@qq.com>
2019-06-17 21:56:49 +0800

    1.添加拙劣的解决方案

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,15 +4,31 @@ import { Form, Input } from 'antd'
 @Form.create()
 export default class Welcome extends React.Component {
   render () {
-    const { form: { getFieldDecorator, getFieldValue } } = this.props
+    const { form: { getFieldDecorator, getFieldValue } } = this.props;
     const getName = () => {
-      console.log(getFieldValue('name'))
+      // 此处获取的值是onChange的前一个值，输入1获取到的是undefine，再输入2时获取到的是1, 再输入2时获取到的是12
+      console.log('name=', getFieldValue('name'))
+    };
+
+    const getName2 = () => {
+      // 此处获取的值是当前值
+      console.log('name2=', getFieldValue('name2'))
     };
 
     return (
-      <Form.Item label="名称">
-        {getFieldDecorator('name')(<Input onChange={getName} />)}
-      </Form.Item>
+      <div>
+        <Form.Item label="名称">
+          {getFieldDecorator('name')(<Input onChange={getName} />)}
+        </Form.Item>
+
+        <Form.Item label="名称2">
+          {getFieldDecorator('name2')(<Input onChange={() => {
+            setTimeout(() => {
+              getName2()
+            })
+          }} />)}
+        </Form.Item>
+      </div>
     )
   }
 }
